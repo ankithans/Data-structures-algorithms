7ea89103dcc7c9ee965650c91e25e3d69718a987
Ankit Hans <ankithans1947@gmail.com>
2020-07-28 22:58:21 +0530

    unordered set done

--- Hashing/hashing.cpp
+++ Hashing/hashing.cpp
@@ -4,14 +4,63 @@ using namespace std;
 
 void inputOutput() {
 #ifndef ONLINE_JUDGE
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
+	freopen("../input.txt", "r", stdin);
+	freopen("../output.txt", "w", stdout);
 #endif
 }
 
 
 int main() {
 	inputOutput();
+
+	unordered_set<int> s;
+	s.insert(10);
+	s.insert(5);
+	s.insert(15);
+	s.insert(20);
+
+	// for (int x : s)
+	// 	cout << x << " ";
+
+	// for (auto it = s.begin(); it != s.end(); it ++)
+	// 	cout << *it << " ";
+
+	// cout << endl;
+	// cout << s.size() << " ";
+	// s.clear();
+	// cout << s.size() << " ";
+
+	// if (s.find(15) == s.end())
+	// 	cout << "Not Found";
+	// else
+	// 	cout << "Found" << *s.find(15);
+
+	// if (s.count(15))
+	// 	cout << "Found";
+	// else
+	// 	cout << "Not Found";
+
+	// cout << s.size() << " ";
+	// s.erase(15);
+	// cout << s.size() << " ";
+	// auto it = s.find(10);
+	// s.erase(it);
+	// cout << s.size() << " ";
+
+	// s.erase(s.begin(), s.end());
+	// cout << s.size();
+
+	// unordered_set uses hashing internally
+	// begin(), end(), cbegin(), cend() - O(1)
+	// insert(), erase(val), erase(it), find(), count - O(1) on average
+	// size(), empty() - O(1)
+
+	// Applications of unordered_set
+	// we can use anywhere when we need following operations
+	// Con a subset of following operations quickly
+	// Search
+	// Insert
+	// Delete
 
 
 
