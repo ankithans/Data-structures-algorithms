9fc1ba011c6c3a8e7687aa71c686eefe1d45835f
Ankit Hans <ankithans1947@gmail.com>
2020-09-08 17:39:08 +0530

    BFS

--- code.cpp
+++ code.cpp
@@ -9,12 +9,21 @@ void inputOutput() {
 #endif
 }
 
+int sum(int a[], int n, int s = 0)
+{
+	if (n < 0)
+		return s;
+	if (a[n] % 2 == 0)
+		s = s + a[n];
+	return sum(a, n - 1, s);
+}
 
 
 int main() {
 	inputOutput();
 
-	cout << "abc";
+	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+	cout << sum(arr, 7, 0);
 
 	return 0;
 }
